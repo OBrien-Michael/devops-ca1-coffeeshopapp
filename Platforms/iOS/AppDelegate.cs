6e664e6ae370eff1004f38dee88d5a24a1931b4e
Michael O'Brien <obrien.michael@outlook.ie>
2024-01-24 19:00:04 +0000

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CoffeeShopApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
